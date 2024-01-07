84163330863e98c23632fbd2ca26189d2b811a7e
Jittapatana Prayoonpruk <52234759+Patrick2544@users.noreply.github.com>
2024-01-06 16:36:19 -0800

    Add files via upload

---
+++ Edge.java
@@ -0,0 +1,13 @@
+public class Edge {
+
+        String u;
+        String v;
+        double weight;
+    
+        public Edge(String u, String v, double weight) {
+            this.u = u;
+            this.v = v;
+            this.weight = weight;
+        }
+    
+}
